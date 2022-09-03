762da02633c9eea6b52fb201851bb6590c903b4c
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-03 19:28:43 +0300

    Added the dummy tests for github actions ci close #6

---
+++ server/src/__tests__/errors.test.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+describe('Dummy tests for ci github actions.', () => {
+  it('2 * 10 equals 4', () => {
+    expect(2 * 10).toBe(20);
+  });
+});
---
+++ server/src/__tests__/users.test.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+describe('Dummy tests for ci github actions.', () => {
+  it('2 * 2 equals 4', () => {
+    expect(2 * 2).toBe(4);
+  });
+});
