0c2dd631804ddbf69542bd9324e0de3cb39776c6
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-04 22:19:52 +0300

    Created the autocomplete controller close #14

---
+++ server/src/controllers/users.js
@@ -0,0 +1,13 @@
+const { getAllUsersQuery } = require('../database/queries/users');
+const CustomizedError = require('../utils/customizedError');
+
+const getAutoCompleteData = (req, res, next) => {
+  const { value } = req.query;
+  getAllUsersQuery(value)
+    .then((data) => res.json(data.rows))
+    .catch(() => next(new CustomizedError(400, 'Bad query request!!!')));
+};
+
+module.exports = {
+  getAutoCompleteData,
+};
