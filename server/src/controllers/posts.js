e1a845f19f9c211aaa7faff5e5e1f9327babfec4
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-04 19:11:52 +0300

    Made the controller return an object containing the rows and the logged in value close #13

--- server/src/controllers/posts.js
+++ server/src/controllers/posts.js
@@ -4,11 +4,17 @@ const CustomizedError = require('../utils/customizedError');
 
 const getAllPostsController = (req, res, next) => {
   const { token } = req.cookies;
-  if (token) res.isLoggedIn = true;
-  else res.isLoggedIn = false;
+  let isLoggedIn = false;
+  if (token) isLoggedIn = true;
 
   getAllPosts()
-    .then((data) => res.json(data.rows))
+    .then((data) => {
+      const returnedObj = {
+        isLoggedIn,
+        rows: data.rows,
+      };
+      res.json(returnedObj);
+    })
     .catch((error) => {
       console.log(error, 'getAllPostsController!!!');
       next(new CustomizedError(400, 'Bad query request!'));
