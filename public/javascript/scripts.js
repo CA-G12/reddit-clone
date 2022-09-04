31d7d3c631e708125106f01f4a4b06d8843f189b
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-04 19:11:03 +0300

    Fetched the data on /api/v1/posts endpoint and created isRedditer functionality, and commented the wordk close #13

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,3 +1,4 @@
+// ? Targeting DOM elements.
 const editIcon = document.querySelector('.edit-icon');
 const closeIcon = document.querySelector('.header-mobile .close-icon');
 const burgerMenu = document.querySelector('.burger-menu-icon');
@@ -5,6 +6,22 @@ const sideMenuCloseIcon = document.querySelector('.mobile-side-menu .close-icon'
 const searchInput = document.querySelectorAll('#search');
 const accountBox = document.querySelector('.account-nav .interface');
 
+// ? Creating loggedInToggle function.
+const loggedInToggle = (isLogged) => {
+  const header = document.querySelector('.header');
+  if (isLogged) {
+    header.classList.toggle('logged-in');
+  }
+};
+
+// ? Create the fetch function to get the posts data.
+fetch('/api/v1/posts')
+  .then((jsonData) => jsonData.json())
+  .then((data) => {
+    loggedInToggle(data.isLoggedIn);
+  });
+
+// ? Creating the event listeners to activate the clickable fields in the page.
 accountBox.addEventListener('click', () => {
   const menu = document.querySelector('.account-nav .menu');
   menu.classList.toggle('hidden');
