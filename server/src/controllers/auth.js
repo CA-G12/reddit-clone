e121be861210ac6c242b59281f6a006de4a69552
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-05 15:52:28 +0300

    Added the login controller and server side validation close #18

---
+++ server/src/controllers/auth.js
@@ -0,0 +1,34 @@
+const { getPasswordForLogin } = require('../database/queries/users');
+const { comparePasswords } = require('../utils/bcrypt');
+const { generateToken } = require('../utils/jwt');
+const { loginValidation } = require('../utils/joiValidation');
+const CustomizedError = require('../utils/customizedError');
+
+const loginController = (req, res, next) => {
+  loginValidation.validateAsync(req.body)
+    .then((validObj) => {
+      getPasswordForLogin(validObj.username)
+        .then((data) => data.rows[0])
+        .then((row) => {
+          comparePasswords(validObj.password, row.password)
+            .then(() => generateToken({ id: row.id, username: req.body.username }, (err, token) => {
+              if (err) next(new CustomizedError(401, 'Wrong Credentials'));
+              res.cookie('token', token).json({ username: req.body.username });
+            }));
+        }).catch((err) => {
+          console.log(err, 'here1');
+          next(new CustomizedError(401, 'Wrong Credentials'));
+        })
+        .catch((err) => {
+          console.log(err, 'here2');
+          next(new CustomizedError(401, 'Wrong Credentials'));
+        });
+    }).catch((err) => {
+      console.log(err, 'here3');
+      next(new CustomizedError(401, 'Wrong Credentials'));
+    });
+};
+
+module.exports = {
+  loginController,
+};
