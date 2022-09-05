f0638835b0f402b6e00431e4066eaf51cb46d9d1
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-06 00:31:16 +0300

    Added signup insert user query close #20

--- server/src/database/queries/users.js
+++ server/src/database/queries/users.js
@@ -16,7 +16,16 @@ const getPasswordForLogin = (username) => {
   return connection.query(sql);
 };
 
+const insertUser = (username, password, email, fname, lname, phone) => {
+  const sql = {
+    text: 'INSERT INTO users(username, password, email, fname, lname, phone) VALUES ($1, $2, $3, $4, $5, $6) RETURNING username',
+    values: [username, password, email, fname, lname, phone],
+  };
+  return connection.query(sql);
+};
+
 module.exports = {
   getAllUsersQuery,
   getPasswordForLogin,
+  insertUser,
 };
