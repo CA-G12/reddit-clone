bfb93a52987b8729af2b3bfa3ef7610e5164887b
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-05 15:53:06 +0300

    Added the login controller to the index file close #18

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,7 +1,9 @@
 const { getAllPostsController } = require('./posts');
 const { getAutoCompleteData } = require('./users');
+const { loginController } = require('./auth');
 
 module.exports = {
   getAllPostsController,
   getAutoCompleteData,
+  loginController,
 };
