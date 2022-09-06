bec63de4c0cd206d5c8028b12de8631fe3a994c9
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-06 22:20:22 +0300

    Made the query return the username also close #24

--- server/src/database/queries/posts.js
+++ server/src/database/queries/posts.js
@@ -1,7 +1,7 @@
 const connection = require('../config/connection');
 
 const getAllPosts = () => {
-  const sql = "SELECT content, votes, users.fname || ' ' || users.lname as full_name FROM posts INNER JOIN users ON posts.user_id = users.id";
+  const sql = "SELECT content, votes, users.fname || ' ' || users.lname as full_name, users.username FROM posts INNER JOIN users ON posts.user_id = users.id";
   return connection.query(sql);
 };
 
