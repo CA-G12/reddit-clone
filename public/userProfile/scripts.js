7cb2653aa330e95da65d6e52191fdccfa0517d87
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-08 23:10:22 +0300

    Finished the front-end functionality for other users pages close #31

--- public/userProfile/scripts.js
+++ public/userProfile/scripts.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-alert */
+
 const searchInputs = document.querySelectorAll('[name="search"]');
 const burgerMenu = document.querySelector('.burger-menu-icon');
 const sideMenuCloseIcon = document.querySelector('.mobile-side-menu .close-icon');
@@ -5,6 +7,170 @@ const loginButtons = document.querySelectorAll('.auth .login');
 const signupFormButtons = document.querySelectorAll('.auth .signup');
 const nextPhase = document.querySelector('.next-section');
 const previousPhase = document.querySelector('.previous-section');
+const emptySearchIcons = document.querySelectorAll('.search-box .close-icon');
+const loginSubmit = document.querySelector('#login-submit');
+const signupSubmit = document.querySelector('.signup-btn');
+
+const getUsername = () => window.location.href.split('=')[1];
+
+const renderUserInfo = (info) => {
+  const name = document.querySelector('.user-info .name');
+  const username = document.querySelector('.user-info .username');
+  const email = document.querySelector('.user-info .email');
+
+  name.textContent = info.name;
+  username.textContent = `u/${info.username}`;
+  email.textContent = `e/${info.email}`;
+};
+
+// ? Create upper vote function.
+const updateVote = (id, kind, uId, pId, e) => {
+  fetch('/api/v1/posts/votes', {
+    method: 'PATCH',
+    headers: {
+      Accept: 'application/json',
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      id,
+      kind,
+      userId: uId,
+      postId: pId,
+    }),
+  })
+    .then((jsonData) => jsonData.json())
+    .then((data) => {
+      if (data.kind === 'upper' && e.target.classList.contains('upper-vote')) {
+        e.target.style.pointerEvents = 'none';
+      }
+      if (data.kind === 'lower' && e.target.classList.contains('lower-vote')) {
+        e.target.style.pointerEvents = 'none';
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+// ? Creating the function which is responsible for getting the votes count.
+const getVotesCount = (id, selector) => {
+  fetch('/api/v1/posts/get-votes', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      id,
+    }),
+  }).then((jsonData) => jsonData.json())
+    .then((data) => {
+      document.querySelector(selector).textContent = data.votes || 0;
+    })
+    .catch((err) => console.log(err));
+};
+
+const createPosts = (array, isLogged) => {
+  const postsContainer = document.querySelector('.posts-container');
+
+  array.forEach((post) => {
+    const postSection = document.createElement('section');
+    postSection.classList.add('post');
+    postsContainer.appendChild(postSection);
+
+    const headSection = document.createElement('section');
+    headSection.classList.add('head');
+    postSection.appendChild(headSection);
+
+    const avatarOnlineSection = document.createElement('section');
+    avatarOnlineSection.classList.add('avatar-online');
+    headSection.appendChild(avatarOnlineSection);
+
+    const userAvatar = document.createElement('img');
+    userAvatar.src = '/assets/avatar-reddit.png';
+    userAvatar.alt = 'user avatar';
+    avatarOnlineSection.appendChild(userAvatar);
+
+    const ballDiv = document.createElement('div');
+    ballDiv.classList.add('posts-online-ball');
+    avatarOnlineSection.appendChild(ballDiv);
+
+    const usernameH3 = document.createElement('h3');
+    usernameH3.classList.add('username');
+    usernameH3.textContent = post.username;
+    headSection.appendChild(usernameH3);
+
+    const followBtn = document.createElement('button');
+    followBtn.type = 'button';
+    followBtn.classList.add('follow-btn');
+    followBtn.textContent = 'Follow';
+    headSection.appendChild(followBtn);
+
+    const title = document.createElement('p');
+    title.classList.add('title');
+    title.textContent = post.title;
+    postSection.appendChild(title);
+
+    const postText = document.createElement('p');
+    postText.classList.add('post-text');
+    postText.textContent = post.content;
+    postSection.appendChild(postText);
+
+    const votesSection = document.createElement('section');
+    votesSection.classList.add('votes');
+    postSection.appendChild(votesSection);
+
+    const upperVote = document.createElement('i');
+    upperVote.className = 'ri-arrow-up-s-line upper-vote';
+    votesSection.appendChild(upperVote);
+
+    const votesCount = document.createElement('h4');
+    votesCount.className = 'vote-number';
+    votesCount.setAttribute('data-id', post.id);
+    votesSection.appendChild(votesCount);
+
+    // ? Calling getVotesCount to get tht votes for the post from the database query.
+    getVotesCount(post.id, `[data-id="${post.id}"]`);
+
+    if (isLogged) {
+      upperVote.addEventListener('click', (e) => {
+        updateVote(post.vote_id, 'upper', post.user_id, post.post_id, e);
+        // ? Updating the votes count.
+        getVotesCount(post.id, `[data-id="${post.id}"]`);
+      });
+    }
+
+    const lowerVote = document.createElement('i');
+    lowerVote.className = 'ri-arrow-down-s-line lower-vote';
+    votesSection.appendChild(lowerVote);
+
+    if (isLogged) {
+      lowerVote.addEventListener('click', (e) => {
+        updateVote(post.vote_id, 'lower', post.user_id, post.post_id, e);
+        // ? Updating the votes count.
+        getVotesCount(post.id, `[data-id="${post.id}"]`);
+      });
+    }
+
+    if (post.kind === 'upper') {
+      upperVote.style.pointerEvents = 'none';
+    }
+    if (post.kind === 'lower') {
+      lowerVote.style.pointerEvents = 'none';
+    }
+
+    const commentsSection = document.createElement('section');
+    commentsSection.classList.add('comments');
+    postSection.appendChild(commentsSection);
+
+    const commentsIcon = document.createElement('i');
+    commentsIcon.className = 'ri-question-answer-fill comments-icon';
+    commentsSection.appendChild(commentsIcon);
+
+    const commentsWord = document.createElement('p');
+    commentsWord.classList.add('comments-text');
+    commentsWord.textContent = 'Comments';
+    commentsSection.appendChild(commentsWord);
+  });
+};
 
 const showSideMenu = (show) => {
   const mobileSideMenu = document.querySelector('.mobile-side-menu');
@@ -31,6 +197,12 @@ const crateAutocomplete = (array) => {
       optionDiv.classList.add('option');
       optionDiv.setAttribute('data-id', row.id);
       ele.appendChild(optionDiv);
+      optionDiv.addEventListener('click', () => {
+        fetch(`/api/v1/users/profile?username=${row.username}`)
+          .then(() => {
+            window.location.href = `/api/v1/users/profile?username=${row.username}`;
+          }).catch((err) => console.log(err));
+      });
 
       const fullName = document.createElement('p');
       fullName.classList.add('full-name');
@@ -60,6 +232,130 @@ burgerMenu.addEventListener('click', () => {
 
 sideMenuCloseIcon.addEventListener('click', () => {
   showSideMenu(false);
+});
+
+// ? Fetching user profile data.
+fetch(`/api/v1/profileInfo?username=${getUsername()}`)
+  .then((jsonData) => jsonData.json())
+  .then((data) => {
+    createPosts(data);
+    renderUserInfo(data[0]);
+  }).catch((err) => console.log(err));
+
+// ? Creating the function which validates the signup form.
+const validateSignup = ({
+  username, password, confirmPassword, email, fname, lname, phone,
+}) => {
+  const isUsernameValid = /^(?=.{5,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/.test(username);
+  const isPasswordValid = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,}$/.test(password);
+  const isPasswordConfirmed = password === confirmPassword;
+  const isEmailValid = /^[a-zA-z0-9]?.*@[a-zA-z0-9]{1,}\.[a-zA-Z]{1,}$/.test(email);
+  const isFnameValid = /^[a-zA-Z]{3,}$/.test(fname);
+  const isLnameValid = /^[a-zA-Z]{3,}$/.test(lname);
+  const isPhoneValid = /[0-9]{10}/.test(phone);
+
+  if (!isUsernameValid) {
+    window.alert('Username must be begin and end with letter or numbers, and be between 5 and 20 characters');
+  }
+  if (!isPasswordValid) {
+    window.alert('Password must contains one lower case letter, one upper case letter, one number and one sign, and be at least 7 characters');
+  }
+  if (!isPasswordConfirmed) {
+    window.alert('Confirm password must the first entered password');
+  }
+  if (!isEmailValid) {
+    window.alert('Email must be a valid email address');
+  }
+  if (!isFnameValid) {
+    window.alert('Name must contains letters only and a minimal length of 3 characters');
+  }
+  if (!isLnameValid) {
+    window.alert('Name must contains letters only and a minimal length of 3 characters');
+  }
+  if (!isPhoneValid) {
+    window.alert('Phone must contains only numbers and must be of a length of 10 characters');
+  }
+
+  return (isUsernameValid && isPasswordValid && isPasswordConfirmed && isEmailValid)
+  && (isPhoneValid && isFnameValid && isLnameValid);
+};
+
+// ? Creating the function which is responsible for validating the form elements.
+const validateLogin = (username, password) => {
+  const isUsernameValid = /^(?=.{5,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/.test(username);
+  const isPasswordValid = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,}$/.test(password);
+
+  if (!isUsernameValid) {
+    window.alert('Username must be begin and end with letter or numbers, and be between 5 and 20 characters');
+  }
+  if (!isPasswordValid) {
+    window.alert('Password must contains one lower case letter, one upper case letter, one number and one sign, and be at least 7 characters');
+  }
+
+  return isPasswordValid && isUsernameValid;
+};
+
+// ? Fetching the login form to login endpoint;
+loginSubmit.addEventListener('click', () => {
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
+  if (validateLogin(username, password)) {
+    fetch('/api/v1/auth/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((jsonData) => jsonData.json())
+      .then((data) => {
+        window.localStorage.setItem('username', data.username);
+        window.localStorage.setItem('id', data.id);
+        return data;
+      })
+      .then(() => {
+        window.location.href = '/';
+      })
+      .catch((err) => console.log(err));
+  }
+});
+
+// ? Fetching the signup form to signup endpoint;
+signupSubmit.addEventListener('click', () => {
+  const loginForm = document.querySelector('.login-form');
+  const username = document.querySelector('#signup-username').value;
+  const password = document.querySelector('#signup-password').value;
+  const confirmPassword = document.querySelector('#confirm-password').value;
+  const email = document.querySelector('#email').value;
+  const fname = document.querySelector('#fname').value;
+  const lname = document.querySelector('#lname').value;
+  const phone = document.querySelector('#phone').value;
+  if (validateSignup({
+    username, password, confirmPassword, email, fname, lname, phone,
+  })) {
+    fetch('/api/v1/auth/signup', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password,
+        email,
+        fname,
+        lname,
+        phone,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(() => {
+        window.location.href = '/';
+        loginForm.style.display = 'flex';
+      })
+      .catch((err) => console.log(err));
+  }
 });
 
 // ? Creating the eventLister to add show the autocomplete on input event
@@ -110,4 +406,19 @@ document.querySelector('.container .auth .login-section .close-icon').addEventLi
 document.querySelector('.signup-section .close-icon').addEventListener('click', () => {
   const LoginFormElement = document.querySelector('.container .auth .inner-signup-cont');
   LoginFormElement.style.display = 'none';
+});
+
+emptySearchIcons.forEach((icon) => {
+  icon.addEventListener('click', (e) => {
+    document.querySelectorAll('.autocomplete').forEach((box) => {
+      const autoBox = box;
+      autoBox.textContent = '';
+      autoBox.style.display = 'none';
+    });
+    searchInputs.forEach((input) => {
+      const search = input;
+      search.value = '';
+    });
+    e.target.style.display = 'none';
+  });
 });
