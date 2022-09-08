22084992af5c96bbe3b239889e59ce93d120d8c2
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-08 23:14:46 +0300

    served the page and served the general data controllers close #32

--- server/src/routes/routing.js
+++ server/src/routes/routing.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const {
   getPostGeneratorPage,
+  getUsersProfile,
 } = require('../controllers');
 const { verifyAccessToken } = require('../utils/jwt');
 
-router.get('/generator', verifyAccessToken, getPostGeneratorPage);
+router.get('/posts/generator', verifyAccessToken, getPostGeneratorPage);
+router.get('/users/profile', getUsersProfile);
 
 module.exports = router;
