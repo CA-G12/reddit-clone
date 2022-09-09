d2fef4720a103a527d2ec58d3c4da1623d94eb1b
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-09 21:39:22 +0300

    Added three new queryies to serve the own profile data controllers close #26

--- server/src/database/queries/posts/insertPost.js
+++ server/src/database/queries/posts/insertPost.js
@@ -2,7 +2,7 @@ const connection = require('../../config/connection');
 
 const insertNewPost = (content, id) => {
   const sql = {
-    text: 'INSERT INTO posts(content, votes, user_id) VALUES($1, 0, $2)',
+    text: 'INSERT INTO posts(title, content, user_id) VALUES($1, 0, $2)',
     values: [content, id],
   };
   return connection.query(sql);
---
+++ server/src/database/queries/posts/postsUpVotedByUser.js
@@ -0,0 +1,36 @@
+const connection = require('../../config/connection');
+
+const getPostsUpVotedByUser = (id) => {
+  const sql = {
+    text: `
+    SELECT
+    posts.id,
+    title,
+    content,
+    votes.id as votes_id,
+    kind,
+    votes.user_id,
+    votes.post_id
+  FROM
+    posts
+  INNER JOIN
+    votes
+  ON
+    posts.id = votes.post_id
+  INNER JOIN
+    users
+  ON
+    votes.user_id = $1
+  WHERE
+    votes.kind = 'upper'
+  GROUP BY
+    posts.id, votes.id
+    `,
+    values: [id],
+  };
+  return connection.query(sql);
+};
+
+module.exports = {
+  getPostsUpVotedByUser,
+};
