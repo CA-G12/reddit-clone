1298f728165a81f8b050dc2c18e6988f517bd122
Mustafa Salem <mustafaissasalem@gmail.com>
2022-09-09 21:41:08 +0300

    Served the page and the general data query close #36

--- server/src/routes/general.js
+++ server/src/routes/general.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
-const { handleProfileInfo } = require('../controllers');
+const { verifyAccessToken } = require('../utils/jwt');
+const { handleProfileInfo, ownProfileData } = require('../controllers');
 
 router.use('/profileInfo', handleProfileInfo);
+router.use('/ownProfileData', verifyAccessToken, ownProfileData);
 
 module.exports = router;
